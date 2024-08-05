cb565d2c4e7e57040d923cfb471c1e3ae1b42974
JavaFxProJect <haru2172@naver.com>
2024-08-05 11:00:23 +0900

    자바 FX 개인 프로젝트 수정

--- src/main/java/com/itgroup/jdbc/InsertMain.java
+++ src/main/java/com/itgroup/jdbc/InsertMain.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class InsertMain { // 노래 생성
     public static void main(String[] args) {
+        // 오늘은 노래를 생성 해볼까?
         MusicDao dao = new MusicDao();
         Music bean = new Music();
 
